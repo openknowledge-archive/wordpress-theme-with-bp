00ae67923a8169ae08db581ad33ca1ffc5ea429f
nigelb <nigelbabu@gmail.com>
2015-02-19 09:39:56 +0530

    Create favicon

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php wp_title( '|', true, 'right' ); ?></title>
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/favicon.ico" />
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <?php wp_head(); ?>
