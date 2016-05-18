ac4d21eca12740c58bf00380e260ead14eaefae1
Goce Mitevski <goce.mitevski@keitaro.info>
2016-05-18 09:08:35 +0000

    Retabed code and removed redundant PHP tags

--- archive.php
+++ archive.php
@@ -6,29 +6,30 @@
  *
  * @package OKFNWP
  */
+get_header();
+?>
 
-get_header(); ?>
+<div class="col-md-8">
+  <?php
+  if (have_posts()) :
+      // Start the Loop.
+      while (have_posts()) : the_post();
 
-	<div class="col-md-8">
-		<?php
-			if ( have_posts() ) :
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
+          /*
+           * Include the post format-specific template for the content. If you want to
+           * use this in a child theme, then include a file called called content-___.php
+           * (where ___ is the post format) and that will be used instead.
+           */
+          get_template_part('content', get_post_type());
 
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_type() );
+      endwhile;
+      // Previous/next post navigation.
+      paging_nav();
 
-				endwhile;
-			// Previous/next post navigation.
-			paging_nav();
+  endif;
+  ?>
+</div>
 
-			endif;
-		?>
-	</div>
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
