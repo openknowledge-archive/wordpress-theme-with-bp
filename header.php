be8f1b1a53a712ad8e75f49bb33da88f40501c1c
Goce Mitevski <goce.mitevski@keitaro.info>
2016-05-23 21:01:40 +0000

    Replaced the static search form code with an include for searchform.php

--- header.php
+++ header.php
@@ -67,18 +67,9 @@
               'container' => '',
               'fallback_cb' => false
           ));
+          
+          get_search_form();
           ?>
-          <form class="navbar-form navbar-right" action="<?php echo home_url(); ?>/" method="get" role="search">
-            <div class="input-group input-group-search">
-              <input type="text" name="s" class="form-control" value="<?php the_search_query(); ?>">
-              <span class="input-group-btn">
-                <button type="submit" class="btn btn-default">
-                  <span class="fa fa-lg fa-search"></span>
-                  <span class="sr-only">Submit</span>
-                </button>
-              </span>
-            </div>
-          </form>
         </div>
       </div>
     </nav>
