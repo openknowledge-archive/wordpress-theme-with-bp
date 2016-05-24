a3a6e2b67c3ab3e8a4d3d4bb1bf6bce1a64b230e
Goce Mitevski <goce.mitevski@keitaro.info>
2016-05-24 12:00:32 +0000

    Added the generic empty content message when no search results have been found

--- search.php
+++ search.php
@@ -4,29 +4,31 @@
  *
  * @package OKFNWP
  */
+get_header();
+?>
 
-get_header(); ?>
+<div class="col-md-8">
+  <?php
+  if (have_posts()) :
+      // Start the Loop.
+      while (have_posts()) : the_post();
 
-	<div class="col-md-8">
-		<?php
-			if ( have_posts() ) :
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
+          /*
+           * Include the post format-specific template for the content. If you want to
+           * use this in a child theme, then include a file called called content-___.php
+           * (where ___ is the post format) and that will be used instead.
+           */
+          get_template_part('content');
 
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content' );
+      endwhile;
+      // Previous/next post navigation.
+      paging_nav();
+  else:
+      get_template_part('content', 'none');
+  endif;
+  ?>
+</div>
 
-				endwhile;
-			// Previous/next post navigation.
-			paging_nav();
-
-			endif;
-		?>
-	</div>
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
