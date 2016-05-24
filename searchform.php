da961e570196ef8bc764799c07a33d578a306369
Goce Mitevski <goce.mitevski@keitaro.info>
2016-05-24 10:53:55 +0000

    Removed .navbar-right from the search form

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
 //The default template for rendering the search form in the header, widgets
 //and everywhere else.
 ?>
-<form class="navbar-form navbar-right" action="<?php echo home_url(); ?>/" method="get" role="search">
+<form class="navbar-form" action="<?php echo home_url(); ?>/" method="get" role="search">
   <div class="input-group input-group-search">
     <input type="text" name="s" class="form-control" value="<?php the_search_query(); ?>">
     <span class="input-group-btn">
