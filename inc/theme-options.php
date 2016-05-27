32337e568d364d7c7f8b8caf7038bfeae121a598
Goce Mitevski <goce.mitevski@keitaro.info>
2016-05-27 15:25:13 +0000

    Commented out the newsletter form settings code in theme-options.php.

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -47,12 +47,12 @@ class OKFNThemeOptions {
             array($this, 'theme_options_sanitize') // sanitize_callback
         );
 
-        add_settings_section(
-            'theme_options_setting_section_mailing', // id
-            __('Mailing List', 'okfnwp'), // title
-            array($this, 'theme_options_section_mailing'), // callback
-            'theme-options-admin' // page
-        );
+//        add_settings_section(
+//            'theme_options_setting_section_mailing', // id
+//            __('Mailing List', 'okfnwp'), // title
+//            array($this, 'theme_options_section_mailing'), // callback
+//            'theme-options-admin' // page
+//        );
 
         add_settings_section(
             'theme_options_setting_section_social', // id
@@ -67,30 +67,29 @@ class OKFNThemeOptions {
 //            array($this, 'theme_options_section_wordpress'), // callback
 //            'theme-options-admin' // page
 //        );
-
-        add_settings_field(
-            "okfnwp_mailinglist_heading", // id
-            __('Mailing List Heading', 'okfnwp'), // title
-            array($this, 'okfnwp_mailinglist_heading_callback'), // callback
-            'theme-options-admin', // page
-            'theme_options_setting_section_mailing' // section
-        );
-
-        add_settings_field(
-            "okfnwp_mailinglist_action", // id
-            __('Mailing URL', 'okfnwp'), // title
-            array($this, 'okfnwp_mailinglist_action_callback'), // callback
-            'theme-options-admin', // page
-            'theme_options_setting_section_mailing' // section
-        );
-
-        add_settings_field(
-            "okfnwp_mailinglist_id", // id
-            __('Mailman List ID', 'okfnwp'), // title
-            array($this, 'okfnwp_mailinglist_id_callback'), // callback
-            'theme-options-admin', // page
-            'theme_options_setting_section_mailing' // section
-        );
+//        add_settings_field(
+//            "okfnwp_mailinglist_heading", // id
+//            __('Mailing List Heading', 'okfnwp'), // title
+//            array($this, 'okfnwp_mailinglist_heading_callback'), // callback
+//            'theme-options-admin', // page
+//            'theme_options_setting_section_mailing' // section
+//        );
+//
+//        add_settings_field(
+//            "okfnwp_mailinglist_action", // id
+//            __('Mailing URL', 'okfnwp'), // title
+//            array($this, 'okfnwp_mailinglist_action_callback'), // callback
+//            'theme-options-admin', // page
+//            'theme_options_setting_section_mailing' // section
+//        );
+//
+//        add_settings_field(
+//            "okfnwp_mailinglist_id", // id
+//            __('Mailman List ID', 'okfnwp'), // title
+//            array($this, 'okfnwp_mailinglist_id_callback'), // callback
+//            'theme-options-admin', // page
+//            'theme_options_setting_section_mailing' // section
+//        );
 
         add_settings_field(
             "okfnwp_twitter_id", // id
@@ -119,17 +118,17 @@ class OKFNThemeOptions {
 
     public function theme_options_sanitize($input) {
         $sanitary_values = array();
-        if (isset($input['okfnwp_mailinglist_heading'])) {
-            $sanitary_values['okfnwp_mailinglist_heading'] = sanitize_text_field($input['okfnwp_mailinglist_heading']);
-        }
-
-        if (isset($input['okfnwp_mailinglist_action'])) {
-            $sanitary_values['okfnwp_mailinglist_action'] = sanitize_text_field($input['okfnwp_mailinglist_action']);
-        }
-
-        if (isset($input['okfnwp_mailinglist_id'])) {
-            $sanitary_values['okfnwp_mailinglist_id'] = sanitize_text_field($input['okfnwp_mailinglist_id']);
-        }
+//        if (isset($input['okfnwp_mailinglist_heading'])) {
+//            $sanitary_values['okfnwp_mailinglist_heading'] = sanitize_text_field($input['okfnwp_mailinglist_heading']);
+//        }
+//
+//        if (isset($input['okfnwp_mailinglist_action'])) {
+//            $sanitary_values['okfnwp_mailinglist_action'] = sanitize_text_field($input['okfnwp_mailinglist_action']);
+//        }
+//
+//        if (isset($input['okfnwp_mailinglist_id'])) {
+//            $sanitary_values['okfnwp_mailinglist_id'] = sanitize_text_field($input['okfnwp_mailinglist_id']);
+//        }
 
         if (isset($input['okfnwp_twitter_id'])) {
             $sanitary_values['okfnwp_twitter_id'] = sanitize_text_field($input['okfnwp_twitter_id']);
@@ -146,9 +145,9 @@ class OKFNThemeOptions {
         return $sanitary_values;
     }
 
-    public function theme_options_section_mailing() {
-        
-    }
+//    public function theme_options_section_mailing() {
+//        
+//    }
 
     public function theme_options_section_social() {
         
@@ -157,46 +156,40 @@ class OKFNThemeOptions {
 //    public function theme_options_section_wordpress() {
 //        
 //    }
+//    public function okfnwp_mailinglist_heading_callback() {
+//
+//        $current_val = $this->theme_options['okfnwp_mailinglist_heading'];
+//        $old_val = get_option('okfnwp_mailinglist_heading');
+//
+//        if (!isset($current_val) && isset($old_val)):
+//            $current_val = $old_val;
+//        endif;
+//
+//        printf('<input class="regular-text" type="text" name="theme_options_option_name[okfnwp_mailinglist_heading]" id="okfnwp_mailinglist_heading" value="%s">', isset($current_val) ? esc_attr($current_val) : '');
+//    }
+//    public function okfnwp_mailinglist_action_callback() {
+//        $current_val = $this->theme_options['okfnwp_mailinglist_action'];
+//        $old_val = get_option('okfnwp_mailinglist_action');
+//
+//        if (!isset($current_val) && isset($old_val)):
+//            $current_val = $old_val;
+//        endif;
+//
+//        printf('<input class="regular-text" type="text" name="theme_options_option_name[okfnwp_mailinglist_action]" id="okfnwp_mailinglist_action" value="%s">', isset($current_val) ? esc_attr($current_val) : '');
+//        _e('<p>URL to which the subscribe form data will be sent. Mailman example: http://lists.okfn.org/mailman/subscribe/XYZ</p>');
+//    }
 
-    public function okfnwp_mailinglist_heading_callback() {
-
-        $current_val = $this->theme_options['okfnwp_mailinglist_heading'];
-        $old_val = get_option('okfnwp_mailinglist_heading');
-
-        if (!isset($current_val) && isset($old_val)):
-            $current_val = $old_val;
-        endif;
-
-        printf('<input class="regular-text" type="text" name="theme_options_option_name[okfnwp_mailinglist_heading]" id="okfnwp_mailinglist_heading" value="%s">', isset($current_val) ? esc_attr($current_val) : '');
-    }
-
-    public function okfnwp_mailinglist_action_callback() {
-        $current_val = $this->theme_options['okfnwp_mailinglist_action'];
-        $old_val = get_option('okfnwp_mailinglist_action');
-
-        if (!isset($current_val) && isset($old_val)):
-            $current_val = $old_val;
-        endif;
-
-        printf('<input class="regular-text" type="text" name="theme_options_option_name[okfnwp_mailinglist_action]" id="okfnwp_mailinglist_action" value="%s">', isset($current_val) ? esc_attr($current_val) : '');
-        ?>
-        <p><?php _e('URL to which the subscribe form data will be sent. Mailman example: http://lists.okfn.org/mailman/subscribe/XYZ'); ?></p>
-        <?php
-    }
-
-    public function okfnwp_mailinglist_id_callback() {
-        $current_val = $this->theme_options['okfnwp_mailinglist_id'];
-        $old_val = get_option('okfnwp_mailinglist_id');
-
-        if (!isset($current_val) && isset($old_val)):
-            $current_val = $old_val;
-        endif;
-
-        printf('<input class="regular-text" type="text" name="theme_options_option_name[okfnwp_mailinglist_id]" id="okfnwp_mailinglist_id" value="%s">', isset($current_val) ? esc_attr($current_val) : '');
-        ?>
-        <p><?php _e('For the list at http://lists.okfn.org/mailman/subscribe/XYZ, this should be XYZ'); ?></p>
-        <?php
-    }
+//    public function okfnwp_mailinglist_id_callback() {
+//        $current_val = $this->theme_options['okfnwp_mailinglist_id'];
+//        $old_val = get_option('okfnwp_mailinglist_id');
+//
+//        if (!isset($current_val) && isset($old_val)):
+//            $current_val = $old_val;
+//        endif;
+//
+//        printf('<input class="regular-text" type="text" name="theme_options_option_name[okfnwp_mailinglist_id]" id="okfnwp_mailinglist_id" value="%s">', isset($current_val) ? esc_attr($current_val) : '');
+//        _e('<p>For the list at http://lists.okfn.org/mailman/subscribe/XYZ, this should be XYZ</p>');
+//    }
 
     public function okfnwp_twitter_id_callback() {
         $current_val = $this->theme_options['okfnwp_twitter_id'];
@@ -231,7 +224,6 @@ class OKFNThemeOptions {
 //
 //        printf('<label for="okfnwp_admin_bar"><input type="checkbox" name="theme_options_option_name[okfnwp_admin_bar]" id="okfnwp_admin_bar" value="1" %s>%s</label>', checked(1, $current_val, false), __(' Show the WordPress Admin toolbar when viewing site', 'okfnwp'));
 //    }
-
 }
 
 if (is_admin()):
