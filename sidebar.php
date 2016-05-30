f80158fbe40bb64b86ee97c66ef73a6626ad55ca
Goce Mitevski <goce.mitevski@keitaro.info>
2016-05-30 16:50:38 +0000

    Included the author-info template at the top of the sidebar.

--- sidebar.php
+++ sidebar.php
@@ -7,6 +7,12 @@
 ?>
 
 <div class="sidebar col-md-4">
+  <?php
+  if (is_single()):
+      // Output the author bio box on post pages
+      get_template_part('author-info');
+  endif;
+  ?>
   <ul>
     <?php dynamic_sidebar('sidebar'); ?>
   </ul>
