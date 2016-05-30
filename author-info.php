bb4fd3d991bfb9accb3d353f30a5c0ecd390c9a3
Goce Mitevski <goce.mitevski@keitaro.info>
2016-05-30 11:33:13 +0000

    Added an anchor to the Gravatar thumbnail, linked to the Gravatar profile page and updated author name in get_the_author_meta() to display_name.

--- author-info.php
+++ author-info.php
@@ -4,17 +4,16 @@
  * 
  * @package OKFNWP
  */
+$avatar = get_avatar(get_the_author_meta('ID'), 100);
+$avatar_url = 'https://www.gravatar.com/' . md5(get_the_author_meta('user_email'));
+$author_description = get_the_author_meta('description');
 
-$avatar = get_avatar(get_the_author(), 100);
-
-if(get_the_author_meta('description')) :
-?>
-
-<aside class="author-info clearfix">
-	<span class="thumbnail"><?php echo $avatar; ?></span>
-	<h4><?php echo sprintf(__('About the author: %1$s'), get_the_author_meta('user_nicename')); ?></h4>
-	<?php echo get_the_author_meta('description'); ?>
-</aside>
-
-<?php
+if ($author_description) :
+    ?>
+    <aside class="author-info clearfix">
+      <a href="<?php echo $avatar_url; ?>"><span class="thumbnail"><?php echo $avatar; ?></span></a>
+      <h4><?php echo sprintf(__('About the author: %1$s'), get_the_author_meta('display_name')); ?></h4>
+      <?php echo $author_description; ?>
+    </aside>
+    <?php
 endif;
