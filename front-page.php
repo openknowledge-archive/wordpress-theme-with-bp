8804d0612946e36f31ba6f6a3e7da2a2356122b3
Goce Mitevski <goce.mitevski@gmail.com>
2016-06-03 18:05:29 +0200

    Modified the implementation of the Featured Categories posts on the Home page, so that only an even number of posts is always shown, as noted in https://github.com/okfn/wordpress-theme/issues/25.

--- front-page.php
+++ front-page.php
@@ -14,63 +14,73 @@ get_header();
   // TO DO: Think of a way for setting this value fixed from the Theme Options
   // in the WordPress admin.
   if (!isset($featured_cat)) {
-      $featured_cat = 'Featured';
+	$featured_cat = 'Featured';
   }
 
   // Check if the Home page is paginated and skip the featured post rendering if it is
   if (isset($paged) && $paged < 2):
-      $featured = new WP_Query([
-          'category_name' => $featured_cat,
-          'posts_per_page' => 1
-      ]);
+	$featured = new WP_Query([
+	  'category_name' => $featured_cat,
+	  'posts_per_page' => 1
+	]);
 
-      if ($featured->have_posts()):
-          while ($featured->have_posts()) : $featured->the_post();
-              get_template_part('content', 'featured');
-          endwhile;
-      else:
-          ?>
-          <div class="alert alert-warning"><p>
-              <?php _e("Please select a Featured posts category in the Theme Options, to see the category's most recent post here.", 'okfnwp'); ?>
-            </p></div>
-      <?php
-      endif;
+	if ($featured->have_posts()):
+	  while ($featured->have_posts()) : $featured->the_post();
+		get_template_part('content', 'featured');
+	  endwhile;
+	  wp_reset_postdata();
+	else:
+	  ?>
+	  <div class="alert alert-warning"><p>
+		  <?php _e("Please select a Featured posts category in the Theme Options, to see the category's most recent post here.", 'okfnwp'); ?>
+		</p></div>
+	<?php
+	endif;
   endif;
 
   // Get the most recent post for each of the featured categories defined in
   // functions.php via okfn_global_vars().
   foreach ($frontpage_categories as $value):
-      $args = [
-          'cat' => $value,
-          'orderby' => 'date',
-          'posts_per_page' => 1,
-          'post__not_in' => $rendered_posts_ids
-      ];
-      $featured_posts[] = new WP_Query($args);
+	$args = [
+	  'cat' => $value,
+	  'orderby' => 'date',
+	  'posts_per_page' => 1,
+	  'post__not_in' => $rendered_posts_ids
+	];
+	$featured_posts[] = new WP_Query($args);
   endforeach;
 
   // Render the most recent post for each of the featured categories,
   // while checking and remembering already rendered posts.
   if ($featured_posts):
-      ?>
-      <div class="row">
-        <?php
-        foreach ($featured_posts as $fp):
+	?>
+    <div class="row">
+	  <?php
+	  foreach ($featured_posts as $key => $fp):
+		if ($fp->have_posts()):
+		  while ($fp->have_posts()):
+			$fp->the_post();
 
-            if ($fp->have_posts()):
-                while ($fp->have_posts()):
-                    $fp->the_post();
-                    if (!okfn_is_post_rendered($post)):
-                        get_template_part('content', 'front');
-                    endif;
-                endwhile;
-            else:
-                get_template_part('content', 'none');
-            endif;
-        endforeach;
-        ?>
-      </div>
-      <?php
+			// Before rendering the post check if its the last one and if its index
+			// is not an even number, don't show it
+			if (($key == sizeof($featured_posts)) && ($key % 2 != 0)):
+			  break;
+			else:
+			  if (!okfn_is_post_rendered($post)):
+				get_template_part('content', 'front');
+			  else:
+				unset($featured_posts[$key]);
+			  endif;
+			endif;
+		  endwhile;
+		  wp_reset_postdata();
+		else:
+		  get_template_part('content', 'none');
+		endif;
+	  endforeach;
+	  ?>
+    </div>
+	<?php
   endif;
   ?>
 </div>
