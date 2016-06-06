a3abacb6235c929d5f4fd3dac3884e7053a65ecf
Goce Mitevski <goce.mitevski@gmail.com>
2016-06-06 11:38:17 +0200

    Switched get_the_post_thumbnail() from 'large' to 'small' for consistency with the other templates.

--- content-blog.php
+++ content-blog.php
@@ -11,7 +11,7 @@ $categories = get_the_category();
       ?>
       <div class="post__thumb">
         <?php
-        echo '<a href="' . get_permalink() . '">' . get_the_post_thumbnail($post, 'large') . '</a>';
+        echo '<a href="' . get_permalink() . '">' . get_the_post_thumbnail($post, 'small') . '</a>';
 
         if ($categories) :
             echo sprintf('<a href="%1$s" class="post__category">%2$s</a>', get_category_link($categories[0]->term_id), $categories[0]->name);
