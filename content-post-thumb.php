fdad24435f3159ee753979737d0d7771bff8da34
Goce Mitevski <goce.mitevski@gmail.com>
2016-08-17 14:58:39 +0200

    Make post thumbs fill its entire container

--- content-post-thumb.php
+++ content-post-thumb.php
@@ -11,7 +11,7 @@ if (has_post_thumbnail()) :
   <div class="post__thumb">
     <?php
 
-    echo '<a href="' . get_permalink() . '">' . get_the_post_thumbnail($post, 'small') . '</a>';
+    echo '<a class="post__thumb-link" href="' . get_permalink() . '">' . get_the_post_thumbnail($post, 'small') . '</a>';
 
     if ($categories) :
       echo sprintf('<a href="%1$s" class="post__category">%2$s</a>', get_category_link($categories[0]->term_id), $categories[0]->name);
@@ -20,8 +20,8 @@ if (has_post_thumbnail()) :
     ?>
   </div>
 <?php else: ?>
-  <div class="post__thumb">
-    <a href="<?php the_permalink(); ?>">
+  <div class="post__thumb post__thumb-default">
+    <a class="post__thumb-link" href="<?php the_permalink(); ?>">
       <img class="attachment-small size-small wp-post-image" src="<?php echo okfn_get_first_image_url_from_post_content(); ?>" alt="">
     </a>
   </div>
