e5ba6ab554a60690be209b2dbef786ebb742f26a
Goce Mitevski <goce.mitevski@gmail.com>
2016-08-18 13:19:15 +0200

    Modify logic in content-post-thumb.php

--- content-post-thumb.php
+++ content-post-thumb.php
@@ -5,17 +5,19 @@
  */
 $categories = get_the_category();
 
-if (!has_post_thumbnail()):
-  $first_post_image = okfn_get_first_image_url_from_post_content();
+if (has_post_thumbnail()) :
+  $thumb = get_the_post_thumbnail($post, 'small');
+else:
+  $thumb = sprintf('<img class="attachment-small size-small wp-post-image" src="%s" alt="">', okfn_get_first_image_url_from_post_content());
 endif;
 
-if (has_post_thumbnail()) :
+if ($thumb) :
 
   ?>
   <div class="post__thumb">
     <?php
 
-    echo '<a class="post__thumb-link" href="' . get_permalink() . '">' . get_the_post_thumbnail($post, 'small') . '</a>';
+    echo '<a class="post__thumb-link" href="' . get_permalink() . '">' . $thumb . '</a>';
 
     if ($categories) :
       echo sprintf('<a href="%1$s" class="post__category">%2$s</a>', get_category_link($categories[0]->term_id), $categories[0]->name);
@@ -23,15 +25,8 @@ if (has_post_thumbnail()) :
 
     ?>
   </div>
-<?php elseif (!empty($first_post_image)): ?>
-  <div class="post__thumb">
-    <a class="post__thumb-link" href="<?php the_permalink(); ?>">
-      <img class="attachment-small size-small wp-post-image" src="<?php echo $first_post_image; ?>" alt="">
-    </a>
-  </div>
 <?php else: ?>
   <div class="post__thumb post__thumb-default">
-    <a class="post__thumb-link" href="<?php the_permalink(); ?>">
-    </a>
+    <a class="post__thumb-link" href="<?php the_permalink(); ?>"></a>
   </div>
 <?php endif;
