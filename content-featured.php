11fd2b6cc3009e294cb53f54b71a173cef8f0914
gmitevski <goce.mitevski@gmail.com>
2016-11-01 14:53:09 +0100

    Make sure the Featured post image is not blurred

--- content-featured.php
+++ content-featured.php
@@ -8,7 +8,7 @@
 
 <div class="post--blog post--featured post--excerpt">
 
-  <?php get_template_part('content-post-thumb'); ?>
+  <?php get_template_part('content-post-thumb-featured'); ?>
 
   <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
   <p class="post__meta"><i class="fa fa-calendar"></i> <?php echo sprintf(__('Posted %1$s', 'okfnwp'), get_the_date()); ?></p>
