2eed70b44812892976fb83556fdbcdde81b26cad
gmitevski <goce.mitevski@gmail.com>
2016-11-04 13:14:36 +0100

    Add missing CSS class to the Featured Post anchor wrap

--- content-post-thumb-featured.php
+++ content-post-thumb-featured.php
@@ -11,7 +11,7 @@ if (has_post_thumbnail()) :
   <div class="post__thumb">
     <?php
 
-    echo '<a href="' . get_permalink() . '">' . get_the_post_thumbnail($post, 'large') . '</a>';
+    echo '<a class="post__thumb-link" href="' . get_permalink() . '">' . get_the_post_thumbnail($post, 'large') . '</a>';
 
     if ($categories) :
       echo sprintf('<a href="%1$s" class="post__category">%2$s</a>', get_category_link($categories[0]->term_id), $categories[0]->name);
