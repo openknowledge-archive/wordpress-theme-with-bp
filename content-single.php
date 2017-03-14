459c776bd7fc595feddb2f08b0e9fa22924f935a
Goce Mitevski <goce.mitevski@gmail.com>
2017-03-14 12:50:01 +0100

    Update content-single.php

--- content-single.php
+++ content-single.php
@@ -5,9 +5,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('post--single'); ?>>
-  <p class="post__meta"><?php the_date(); ?>, <?php echo __('by', 'okfnwp') . sprintf(' <a href="%1$s">%2$s</a>', get_author_posts_url(get_the_author_meta('ID')), get_the_author_meta('display_name')); ?></p>
+  <p class="post__meta"><?php the_date(); ?>, <?php echo __('by', 'okfnwp'); ?>
+    <?php
+    // Check if the Co-authrors plugin is available
+    if (function_exists('coauthors_posts_links')) {
+      coauthors_posts_links();
+    } else {
+      the_author_posts_link();
+    }
+    ?></p>
   <div class="entry-content">
-	<?php the_content(); ?>
+    <?php the_content(); ?>
   </div>
   <footer class="entry-footer">
     <span class="cat-links"><?php echo __('Posted in:', 'okfnwp') . ' ' . get_the_category_list(_x(', ', 'Used between list items, there is a space after the comma.', 'okfnwp')); ?></span>
