9d6e7a74c6519147ad90209eed34af54d3d7ec4a
Goce Mitevski <goce.mitevski@gmail.com>
2017-03-14 12:49:46 +0100

    Update author-info.php

--- author-info.php
+++ author-info.php
@@ -1,22 +1,27 @@
 <?php
 /**
  * Display the author's name and bio within blog posts
- * 
+ *
  * @package OKFNWP
  */
-$author = get_the_author();
-$author_description = get_the_author_meta('description');
-$avatar = get_avatar(get_the_author_meta('ID'), 80);
+if (function_exists('coauthors')) :
+  get_template_part('author', 'info-coauthors');
+else:
 
-// Prepare a suitable URL for the Gravatar user profile, as recommended here:
-// https://en.gravatar.com/site/implement/profiles/
-$avatar_url = 'https://www.gravatar.com/' . md5(get_the_author_meta('user_email'));
+  $author = get_the_author();
+  $author_description = get_the_author_meta('description');
+  $avatar = get_avatar(get_the_author_meta('ID'), 80);
 
-if (empty($author_description)):
+  /* Prepare a suitable URL for the Gravatar user profile, as recommended here:
+   *  https://en.gravatar.com/site/implement/profiles/
+   */
+  $avatar_url = 'https://www.gravatar.com/' . md5(get_the_author_meta('user_email'));
+
+  if (empty($author_description)):
     $author_description = __('No description set for this author.', 'okfnwp');
-endif;
+  endif;
 
-if ($author):
+  if ($author):
     ?>
     <aside class="author-info clearfix">
       <a href="<?php echo $avatar_url; ?>"><span class="thumbnail"><?php echo $avatar; ?></span></a>
@@ -24,4 +29,6 @@ if ($author):
       <p><?php echo $author_description; ?></p>
     </aside>
     <?php
+  endif;
+  
 endif;
